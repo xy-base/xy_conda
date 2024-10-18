aeccb4f0f623e52df08512bd4c193d3778a5615b
yuyanget <845262968@qq.com>
2024-10-18 20:43:53 +0800

    release 1.0.3

---
+++ xy_conda/execute/Main.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'helios'
+__doc__ = 'Main'
+'''
+  * @File    :   Main.py
+  * @Time    :   2023/06/03 08:10:13
+  * @Author  :   helios
+  * @Version :   1.0
+  * @Contact :   yuyang.0515@qq.com
+  * @License :   (C)Copyright 2019-2023, Ship of Ocean
+  * @Desc    :   None
+'''
+
+from xy_conda.Runner import Runner
+
+
+def main():
+    runner = Runner()
+    runner.main()
+
+
+if __name__ == "__main__":
+    main()
